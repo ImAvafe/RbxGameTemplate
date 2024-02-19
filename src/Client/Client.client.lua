31cf3d8c81c1d6ce94caf14831eff3ba7d608001
Avafe <65048459+ImAvafe@users.noreply.github.com>
2024-02-19 12:53:23 -0500

    update

--- src/Client/Client.client.lua
+++ src/Client/Client.client.lua
@@ -1,15 +1,19 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local StarterPlayerScripts = game:GetService("StarterPlayer"):WaitForChild("StarterPlayerScripts")
+local StarterPlayer = game:GetService("StarterPlayer")
 
 local Knit = require(ReplicatedStorage.Packages.Knit)
 local Loader = require(ReplicatedStorage.Packages.Loader)
 
-Knit.AddControllers(StarterPlayerScripts.Controllers)
+Loader.LoadDescendants(StarterPlayer.StarterPlayerScripts.Controllers, function(Descendant)
+	if Descendant:IsA("ModuleScript") and Descendant.Name:match("Controller$") ~= nil then
+		return Knit.CreateController(require(Descendant))
+	end
+end)
 
 Knit.Start():andThen(function()
-   print('KnitStart client!')
+	print("KnitStart client!")
 
-   Loader.LoadDescendants(StarterPlayerScripts.Components, function(Descendant)
-      return Descendant:IsA("ModuleScript")
-   end)
+	Loader.LoadDescendants(StarterPlayer.StarterPlayerScripts.Components, function(Descendant)
+		return Descendant:IsA("ModuleScript")
+	end)
 end)
--- src/Server/Server.server.lua
+++ src/Server/Server.server.lua
@@ -4,12 +4,16 @@ local ServerScriptService = game:GetService("ServerScriptService")
 local Knit = require(ReplicatedStorage.Packages.Knit)
 local Loader = require(ReplicatedStorage.Packages.Loader)
 
-Knit.AddServices(ServerScriptService.Services)
+Loader.LoadDescendants(ServerScriptService.Services, function(Descendant)
+	if Descendant:IsA("ModuleScript") and Descendant.Name:match("Service$") ~= nil then
+		return Knit.CreateService(require(Descendant))
+	end
+end)
 
 Knit.Start():andThen(function()
-   print('KnitStart server!')
+	print("KnitStart server!")
 
-   Loader.LoadDescendants(ServerScriptService.Components, function(Descendant)
-      return Descendant:IsA("ModuleScript")
-   end)
+	Loader.LoadDescendants(ServerScriptService.Components, function(Descendant)
+		return Descendant:IsA("ModuleScript")
+	end)
 end)
